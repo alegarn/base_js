6cf77752bffc6db0364f972a4649add5b62f122c
alegarn <alexgarnier78310@protonmail.com>
2022-11-09 22:18:54 +0100

    no refacto modal, with movie infos

--- json_async_api/scripts/script.js
+++ json_async_api/scripts/script.js
@@ -13,11 +13,46 @@ var options = {
 
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
-
+      console.log(entry.target);
       if (entry.isIntersecting) {
-        const htmlImg = entry.target.firstElementChild;
+        const htmlImg = entry.target.getElementsByClassName("img-fluid")[0];
+        console.log(htmlImg);
         const url = window.location.href;
         htmlImg.src = `${htmlImg.src}`.replace(`${url}+++`, "");
+
+        const htmlbtn = entry.target.querySelector(".popup");
+        const modal = entry.target.querySelector(".modal");
+        const span = modal.querySelector(".close");
+        console.log(htmlbtn);
+        console.log(modal);
+        console.log(span);
+
+        htmlbtn.addEventListener("click", (btn) => {
+          const title = btn.target.parentNode.querySelector(".card-title");
+
+          modal.style.display = "block";
+          fetch(`http://www.omdbapi.com/?i=${ID}&apikey=${API_KEY}&type=movie&r=json&t=${title.textContent}&plot=full`).then((response) => { return response.json(); })
+          .then((response) => {
+            const modalBody = modal.querySelector(".modal-body");
+            console.log(modalBody);
+            const genre = modalBody.childNodes[1];
+            console.log(genre);
+            genre.textContent = `${response.Genre}`;
+            const director =  modalBody.childNodes[5];
+            director.textContent = `${response.Director}`;
+            const runtime = modalBody.childNodes[7];
+            runtime.textContent = `${response.Runtime}`;
+            const plot = modalBody.childNodes[3];
+            plot.textContent = `${response.Plot}`;
+
+          })
+          .catch((error) => { console.error(error); });
+          console.log(btn.target);
+        })
+
+        span.onclick = function() {
+          modal.style.display = "none";
+        }
       };
       
       entry.target.classList.toggle("show", entry.isIntersecting);
@@ -46,11 +81,36 @@ function renderMovie(scrollArea, returnedList) {
     const newSection = document.createElement("div");
 
     newSection.innerHTML = `\
-      <img src="+++${movie.Poster}" class="card-img-top" alt="..."> \
-      <div class="card-body"> \
-        <h5 class="card-title">${movie.Title}</h5> \
-        <p class="card-text">${movie.Year}</p> \
-        <div class="btn btn-primary popup">Read more</div> \
+      <div class="row g-0"> \
+        <div class="col-md-4"> \
+          <img src="+++${movie.Poster}" class="img-fluid rounded-start"> \
+        </div> \
+        <div class="col-md-8 center"> \
+          <div class="card-body"> \ 
+            <h5 class="card-title">${movie.Title}</h5> \
+            <p class="card-text">${movie.Year}</p> \
+            <div class="btn btn-primary popup">Read more</div> \ 
+          </div> \
+        </div> \
+      </div> \
+        \
+      <div class="modal position-absolute top-50 start-50 translate-middle"> \
+        <div class="modal-content"> \
+          <div class="modal-header"> \
+            <span class="close">&times;</span> \
+            <h2 class="title">${movie.Title}</h2> \
+          </div> \
+          <div class="modal-body"> \
+            <p class="genre"></p>
+            <p class="plot"></p> \
+            <p class="director"></p>
+            <p class="runtime"></p>
+          </div> \
+          <div class="modal-footer"> \
+            <h3>${movie.Year}</h3> \
+          </div> \
+        </div> \
+      </div> \
       `;
 
     newSection.classList.add("slide-card");
@@ -65,24 +125,24 @@ let callbackFunction = formSubmit.addEventListener('click', (e) => {
 
   const submitName =filmName.value;
 
-  fetch(`http://www.omdbapi.com/?i=${ID}&apikey=${API_KEY}&type=movie&r=json&page=5&s=${submitName}`).then((response) => { return response.json(); })
+  fetch(`http://www.omdbapi.com/?i=${ID}&apikey=${API_KEY}&type=movie&r=json&page=5&s=${submitName}&plot=full`).then((response) => { return response.json(); })
   .then((response) => {
     console.log(response);
     const returnedList = response.Search;
 
     renderMovie(scrollArea, returnedList);
 
-    intersectionFilm.observe(document.querySelector(".slide-card:last-child"));
-
     const scrollCards = document.querySelectorAll(".slide-card");
 
     scrollCards.forEach( card => {
       observer.observe(card);
-
     });
+
+    intersectionFilm.observe(document.querySelector(".slide-card:last-child"));
     
   })
   .catch((error) => { console.error(error); });
   console.log(e.target);
 
 });
+
